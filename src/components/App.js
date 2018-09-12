9ca578550e433dc54905d28fab7986e0471816f1
JenLindsay <JenLindsay@users.noreply.github.com>
2018-09-11 21:47:57 -0700

    updated app description

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
                 <div className="header">
                     <div className="container">
                         <div className="header__title">{this.props.componentName}</div>
-                        <div className="warning">NOTE: this code currently uses a CORS proxy to access the Harvest API server</div>
+                        <div className="warning">NOTE: app currently uses a CORS proxy to access the Harvest API server</div>
                     </div>                   
                 </div> 
                 <UsersList />
