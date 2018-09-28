569487794ed3c9b6b9a25f6d44bb4198c9f3d770
JenLindsay <JenLindsay@users.noreply.github.com>
2018-09-27 22:01:27 -0700

    enabled spinner and modified setState calls

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -15,25 +15,27 @@ class SignIn extends React.Component {
     };
 
     onClick = (e) => {
-        this.state.showSpinner = true;
+        this.setState({ showSpinner: true });
         this.props.signin(this.state.passcode, () => {
-                this.state.showSpinner = false;
+            this.setState({ showSpinner: false });
         });
     };
 
     render() {
         return (
-            <div className="container__centered--column">
-                <label className="widget__message container__centered--grow">Enter passcode for access:</label>
-                <input className="container__centered--grow"
-                    type="password"
-                    autoFocus
-                    value={this.state.passcode}
-                    onChange={this.onPasscodeChange}
-                />
-                {this.props.errorMessage && <div className="error">{this.props.errorMessage}</div>}
-                <button className="button container__centered--grow" onClick={this.onClick}>Enter</button>
-            </div>
+            <Spinner active={!(!this.state.showSpinner || (this.state.showSpinner && this.props.errorMessage))} spinner text='Signing you in...'>
+                <div className="container__centered--column">
+                    <label className="widget__message container__centered--grow">Enter passcode for access:</label>
+                    <input className="container__centered--grow"
+                        type="password"
+                        autoFocus
+                        value={this.state.passcode}
+                        onChange={this.onPasscodeChange}
+                    />
+                    {this.props.errorMessage && <div className="error">{this.props.errorMessage}</div>}
+                    <button className="button container__centered--grow" onClick={this.onClick}>Enter</button>
+                </div>
+            </Spinner>
         );
     }
 }
